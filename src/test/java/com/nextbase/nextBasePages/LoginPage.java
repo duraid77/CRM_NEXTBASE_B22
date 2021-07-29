ebe4905b83d44ee399411457b4a86bf9a79cae9b
Berk Ogutal <ogutalberk@gmail.com>
2021-07-29 12:35:06 -0400

    US2_TS-002_Berk first commit

---
+++ src/test/java/com/nextbase/nextBasePages/LoginPage.java
@@ -0,0 +1,23 @@
+package com.nextbase.nextBasePages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement usernameBox;
+
+    @FindBy(name = "USER_PASSWORD")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginButton;
+
+    public void loginMethod(String username, String password){
+        usernameBox.sendKeys(username);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+
+    }
+}
--- src/test/java/com/nextbase/nextBasePages/nextBase_Pages.java
+++
@@ -1,4 +0,0 @@
-package com.nextbase.nextBasePages;
-
-public class nextBase_Pages {
-}
